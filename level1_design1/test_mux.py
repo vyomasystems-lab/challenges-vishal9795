b9659f9f37b5f286d3e140d7193d3c7137808860
vishal9795 <vishal.sistec1994@gmail.com>
2022-07-27 17:55:36 +0000

    updated  test file

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,4 +7,32 @@ from cocotb.triggers import Timer
 async def test_mux(dut):
     """Test for mux2"""
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    I1 = 3
+    SEL1 = 1 
+
+    #input driving
+    dut.inp1 = I1
+    dut.sel = SEL1
+
+    await Timer(2, units='ns') 
+
+    assert dut.out.value = I1(SEL1),f" muxout is incorrect:{dut.X.value}! = 3"
+    @cocotb.test()
+async def mux_randomised_test(dut):
+    """Test for mux 2 random numbers multiple times"""
+
+    for i in range(5):
+
+        I1 = random.randint(0, 15)
+        SEL1 = random.randint(0, 15)
+
+        dut.inp1.value = I1
+        dut.sel.value = SEL1
+
+        await Timer(2, units='ns')
+        
+        dut._log.info(f'I1={I1:05} SEL1={SEL1:05} model={I1(SEL1):05} DUT={int(dut.out.value):05}')
+        assert dut.out.value == I1(SEL1), "Randomised test failed with: {I1}({SEL1}) = {OUT}".format(
+            A=dut.inp1.value, sel=dut.sel.value, out=dut.out.value)
+
+
